9f61131c1282f0db7cc310f582e96ea9a206cda5
Robert <pinski1@gmail.com>
2017-11-30 10:59:12 +0000

    Update rainbow.py

--- Software/rainbow.py
+++ Software/rainbow.py
@@ -6,6 +6,30 @@ class Rainbow_Challenge(Challenge):
     
     def run(self):
         print("Rainbow Road")
+        
+        """
+        steps:
+            turn 45degrees
+            drive to one corner
+                keep ?? mm from side wall
+                until ?? mm from front wall
+            turn 135degrees
+            drive to next corner
+                keep ?? mm from side wall
+                until ?? mm from front wall
+            turn 90 degrees
+            drive to next corner
+                keep ?? mm from side wall
+                until ?? mm from front wall
+            turn 90 degrees
+            drive to next corner
+                keep ?? mm from side wall
+                until ?? mm from front wall
+            turn 45 degrees
+            drive to center
+                until ?? mm traveled
+            turn 45 degrees
+        """
     
 
 if __name__ == "__main__":
