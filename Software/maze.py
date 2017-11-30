c464291ca6fd129033294c0990eb15e531ced2a3
Robert <pinski1@gmail.com>
2017-11-30 10:42:54 +0000

    Update maze.py

--- Software/maze.py
+++ Software/maze.py
@@ -7,6 +7,31 @@ class Maze_Challenge(Challenge):
     def run(self):
         print("Maze")
         print("Input device is" + self.input_device)
+        
+        """
+        steps:
+        drive forward
+            distance ?? mm from left wall
+            until ?? mm from forward wall
+        drive 90degree right
+        drive foward
+            distance ?? mm from left wall
+            until ?? mm from forward wall
+        drive 180degree right
+        drive forward
+            distance ?? mm from left wall
+            until ?? mm from forward wall
+        drive 180degree left
+        drive forward
+            distance ?? mm from left wall
+            until ?? mm from forward wall
+        drive 90degree left
+        drive foward
+            distance ?? mm from left wall
+            when left wall disappears, continue for 200mm
+        stop
+        """
+        
         #return
         pass
 
